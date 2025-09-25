f1dfaddd8b5fb2058fc1e09d8630a5d52186f980
MatheusFaganelli <aulaspython2025@gmail.com>
2025-09-25 14:34:19 -0300

    exercicios sobre lista

---
+++ Modulo 1/Listas/01lista.py
@@ -0,0 +1,16 @@
+numeros = [1,2,3,4,5,6,7]
+
+print(type(numeros)) # o comando type informa o tipo da variavel
+
+print(numeros[1]) # função para buscar um elemento especific da lista
+
+print(numeros[-1])#para cnhecermos o ultimo elemento de uma lista
+
+numeros[2] = 10  #substitui um elemento especifico da lista
+print(numeros)
+
+numeros.append(6)  # adicionando um elemento que estava no indice 2
+print(numeros)
+
+numeros.V=remove(4)
+print (numeros)
