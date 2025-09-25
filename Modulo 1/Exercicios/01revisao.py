e302bb578e80253f6082920f3140d140b9b64556
MatheusFaganelli <aulaspython2025@gmail.com>
2025-09-25 14:24:40 -0300

    exercicios variados

---
+++ Modulo 1/Exercicios/01revisao.py
@@ -0,0 +1,5 @@
+#crie um programa que receba dois numeros e exiba o resultado a soma, subtração, multiplicação e divisao
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
---
+++ Modulo 1/Exercicios/02revisao.py
@@ -0,0 +1,7 @@
+#escreva um programa que 
+# verifique se um numero é par ou impar usando o operador de modulo%
+
+
+
+
+
